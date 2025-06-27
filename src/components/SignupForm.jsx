e7968de91f69f3e292cdeeacb05be8df2c828aa0
Laassel <lassel.giovanni@gmail.com>
2025-06-27 12:36:06 +0200

    Add controlled input for full name with useState

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
 function SignupForm () {
+    // Stato per il nome completo
+    const [fullName, setFullName] = useState("");
+
+    // Handler per aggiornare lo stato 
+    const handleFullName = (e) => {
+        setFullName(e.target.value);
+        console.log("Nome completo:", e.target.value)
+    };
 
     return (
         <form>
             <h2>Registrati alla piattaforma</h2>
+
+            {/* Campo input controllato */}
+            <div>
+                <label htmlFor="fullName">Nome completo:</label>
+                <input 
+                type="text" 
+                id="fullName"
+                value={fullName}
+                onChange={handleFullName}
+                />
+            </div>
+
         </form>
     );
 }
